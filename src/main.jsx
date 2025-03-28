c3c23acde2b68efba12432627edf130b3adb4dc2
Lena_Bondar <lencha.2005@gmail.com>
2025-03-28 18:08:56 +0200

    add persist for theme

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { persistor, store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <Provider store={store}>
     <BrowserRouter>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </BrowserRouter>
+      </PersistGate>
   </Provider>
+    </BrowserRouter>
   </StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,39 @@ import { authReducer } from './auth/slice';
 import { psychologistsReducer } from './psychologists/slice';
 import { themeReducer } from './theme/slice';
 import { modalReducer } from './modal/slice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "theme",
+  storage,
+  whitelist: ["theme"],
+};
+
+const persistedThemeReducer = persistReducer(persistConfig, themeReducer);
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     psychologists: psychologistsReducer,
-    theme: themeReducer,
     modal: modalReducer,
+    theme: persistedThemeReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
